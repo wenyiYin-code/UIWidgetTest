4dfd1ebaf9b44fab0f7b32be49d1a52d691d1fe3
wenyiYin <443644234@qq.com>
2023-10-10 17:57:19 +0800

    2.提交Button控件，通过if-else来判断

--- app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.uiwidgettest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
     }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.button) {
+            Toast.makeText(MainActivity.this, "you clicked button",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
