3eab32200344573e6da7d74b0c8480a7d5c547f8
wenyiYin <443644234@qq.com>
2023-10-11 10:57:54 +0800

    6.新增ProgresBar控件 7.新增通过按钮控制进度条显示逻辑功能

--- app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Handler handler = new Handler();
     private EditText editText;
     private ImageView imageView;
+    private ProgressBar progressBar;
     private final String TAG = "MainActivity";
 
     @Override
@@ -25,9 +29,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
         imageView = (ImageView) findViewById(R.id.image_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.GONE);
         button.setOnClickListener(this);
     }
-
 
     @Override
     public void onClick(View v) {
@@ -35,14 +40,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String inputText = editText.getText().toString();
             Log.d(TAG, "inputText:" + inputText);
             if(inputText.equals("img_1")) {
+                progressBar.setVisibility(View.VISIBLE);
+                hideProgressBar();
                 imageView.setImageResource(R.drawable.img_1);
             }
-            if(inputText.equals("img_2")){
+            if(inputText.equals("img_2")) {
+                progressBar.setVisibility(View.VISIBLE);
+                hideProgressBar();
                 imageView.setImageResource(R.drawable.img_2);
             }
             Toast.makeText(MainActivity.this, inputText,
                     Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void hideProgressBar(){
+        long delayMillis = 2000; // 2秒
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                progressBar.setVisibility(View.GONE);
+            }
+        }, delayMillis);
     }
 
 }
